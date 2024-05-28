acd7e97fb240e70e90fd24fcea91ba3037fa66ee
Raj Karmakar <97rajmath@gmail.com>
2024-05-28 11:39:56 +0530

    Create a new instance of Web3 with ganache provider

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,5 +3,6 @@ const { Web3 } = require('web3');
 const assert = require('assert')
 
 // updated ganache and web3 imports added for convenience
+const web3 = new Web3(ganache.Provider())
 
 // contract test code will go here
